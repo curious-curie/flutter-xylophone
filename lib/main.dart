55e11a35d7158a8b3d55558f56a8b08fef4cbe2a
curious-curie <curious.curieyoo@gmail.com>
2020-04-19 18:55:37 +0900

    make buttons to play diff sound

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,76 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-              child: FlatButton(
+      home: Scaffold(body: SafeArea(child: Buttons())),
+    );
+  }
+}
+
+class Buttons extends StatelessWidget {
+  void playSound(int number) {
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          FlatButton(
+            color: Colors.red,
+            onPressed: () {
+              playSound(1);
+            },
+          ),
+          FlatButton(
+            color: Colors.orange,
             onPressed: () {
               final player = AudioCache();
-              player.play('note1.wav');
+              playSound(2);
             },
-            child: Text('Click Me'),
-          )),
-        ),
+          ),
+          FlatButton(
+            color: Colors.yellow,
+            onPressed: () {
+              playSound(3);
+            },
+          ),
+          FlatButton(
+            color: Colors.lightGreen,
+            onPressed: () {
+              playSound(4);
+            },
+          ),
+          FlatButton(
+            color: Colors.green,
+            onPressed: () {
+              playSound(5);
+            },
+          ),
+          FlatButton(
+            color: Colors.blue,
+            onPressed: () {
+              playSound(6);
+            },
+          ),
+          FlatButton(
+            color: Colors.purple,
+            onPressed: () {
+              playSound(7);
+            },
+          )
+        ],
       ),
     );
   }
 }
+
+//
+//child: FlatButton(
+//onPressed: () {
+//final player = AudioCache();
+//player.play('note1.wav');
+//},
+//child: Text('Click Me'),
+//)),
